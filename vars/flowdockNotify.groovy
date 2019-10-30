74d2af3d3d696d7aaadb8be638821c74f9144a5e
Andrew <H2wk@users.noreply.github.com>
2019-10-30 15:59:36 +0000

    support for inbox or message posting

--- vars/flowdockNotify.groovy
+++ vars/flowdockNotify.groovy
@@ -5,11 +5,9 @@ import groovy.json.JsonOutput
 import java.net.URLEncoder
 import hudson.model.Result
 
-def call(script, flowToken, tags = '') {
+def call(script, flowToken, tags = '', type) {
 
     tags = tags.replaceAll("\\s","")
-
-    def flowdockURL = "https://api.flowdock.com/messages"
 
     // build status of null means successful
     def buildStatus =  script.currentBuild.result ? script.currentBuild.result : 'SUCCESS'
@@ -20,6 +18,8 @@ def call(script, flowToken, tags = '') {
     // we use the build+XX@flowdock.com addresses for their yay/nay avatars
     def fromAddress = ''
 
+    def colorStatus = ''
+
     // update subject and set from address based on build status
     switch (buildStatus) {
       case 'SUCCESS':
@@ -27,47 +27,87 @@ def call(script, flowToken, tags = '') {
         if (Result.FAILURE.toString().equals(prevResult) || Result.UNSTABLE.toString().equals(prevResult)) {
           subject += ' was fixed'
           fromAddress = 'build+ok@flowdock.com'
+          colorStatus = 'green'
           break
         }
         subject += ' was successful'
         fromAddress = 'build+ok@flowdock.com'
+        colorStatus = 'green'
         break
       case 'FAILURE':
         subject += ' failed'
         fromAddress = 'build+fail@flowdock.com'
+        colorStatus = 'red'
         break
       case 'UNSTABLE':
         subject += ' was unstable'
         fromAddress = 'build+fail@flowdock.com'
+        colorStatus = 'yellow'
         break
       case 'ABORTED':
         subject += ' was aborted'
         fromAddress = 'build+fail@flowdock.com'
+        colorStatus = 'grey'
         break
       case 'NOT_BUILT':
         subject += ' was not built'
         fromAddress = 'build+fail@flowdock.com'
+        colorStatus = 'grey'
         break
       case 'FIXED':
         subject = ' was fixed'
         fromAddress = 'build+ok@flowdock.com'
+        colorStatus = 'green'
         break
     }
 
-    // build message
-    def content = """${script.currentBuild.fullDisplayName}
-      Result: ${buildStatus}
-      URL: ${script.env.BUILD_URL}
-      Author: ${script.env.GIT_COMMITTER_NAME}
-      Commit: ${script.env.GIT_COMMIT}"""
+    def payload
 
-    // build payload
-    def payload = JsonOutput.toJson([
+    if (type = 'inbox') {
+        // Post is going into the flow as an inbox message
+
+         def content = """<h3>${script.env.JOB_BASE_NAME} ${subject}</h3>
+           Build: ${script.currentBuild.displayName}<br />
+           Result: <strong>${buildStatus}<br />
+           URL: <a href="${script.env.BUILD_URL}">${script.currentBuild.fullDisplayName}</a><br />"""
+
+            payload = JsonOutput.toJson([
+                     flow_token: flowToken,
+                     event: 'activity',
+                     title: script.env.JOB_BASE_NAME,
+                     body: content,
+                     external_thread_id: script.env.GIT_COMMIT,
+                     "status": [
+                             "color": colorStatus,
+                             "value": buildStatus
+                     ],
+                     author: [
+                         name : script.env.GIT_COMMITTER_NAME,
+                         email: fromAddress
+                     ]
+                    ])
+
+    } else {
+        // Post is going into flow as a chat message
+        def content = """${script.env.JOB_BASE_NAME} ${subject}
+            Result: ${buildStatus}
+            Build: ${script.currentBuild.displayName}
+            URL: ${script.env.BUILD_URL}
+            Author: ${script.env.GIT_COMMITTER_NAME}
+            Commit: ${script.env.GIT_COMMIT}"""
+
+        // build payload
+        payload = JsonOutput.toJson([
                 flow_token: flowToken,
                 event: 'message',
                 content: content,
                 tags:tags
-                ])
+        ])
+    }
+
+
+
+
 
     // craft and send the request
     def post = new URL(flowdockURL).openConnection();
